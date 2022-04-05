2f90773b9e01295ca5a925a740853cb195792e0e
Khyra <khyrak@gmail.com>
2022-04-05 17:04:22 -0300

    req 3 feito

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Play from './pages/Play';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route exact path="/play" component={ Play } />
+      <Route path="/settings" component={ Settings } />
     </Switch>
   );
 }
