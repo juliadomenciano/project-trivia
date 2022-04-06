d7ddfea23e2552bf02fdb65baad556f429a43900
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-04-06 19:39:09 -0300

    ..

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,29 +5,30 @@ import PropTypes from 'prop-types';
 class Question extends React.Component {
   constructor() {
     super();
-    this.state = {
-
-    };
+    this.state = {};
   }
 
   render() {
     const { askQuestion } = this.props;
     console.log(askQuestion.question);
-    const respostas = [...askQuestion.incorrect_answer, askQuestion.correct_answer];
+    const respostas = [
+      ...askQuestion.incorrect_answer,
+      askQuestion.correct_answer,
+    ];
     return (
       // corretc_answer
       // incorret_answer
       <section>
-        <h1 data-testid="question-category">{ askQuestion.category }</h1>
-        <h3 data-testid="question-text">{ askQuestion.question }</h3>
+        <h1 data-testid="question-category">{askQuestion.category}</h1>
+        <h3 data-testid="question-text">{askQuestion.question}</h3>
         <div data-testid="answer-options">
-          {
-            respostas.map((item, index) => {
-              item === askQuestion.correct_answer ? (
-              <button data-testid="correct-answer">{ item }</button>
-              ) : <button data-testid={`wrong-answer-${index}`}>{ item }</button>
-            })
-          }
+          {respostas.map((item, index) => {
+            item === askQuestion.correct_answer ? (
+              <button data-testid="correct-answer">{item}</button>
+            ) : (
+              <button data-testid={ `wrong-answer-${index}` }>{item}</button>
+            );
+          })}
         </div>
       </section>
     );
